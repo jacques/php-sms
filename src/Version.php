e3addf3556a8cb1cb64e7bd3876560d9fb9759c8
Jacques Marneweck <jacques@siberia.co.za>
2020-12-10 21:01:56 +0200

    Add class to hold the version number

---
+++ src/Version.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * PHP SMS
+ *
+ * @author    Jacques Marneweck <jacques@siberia.co.za>
+ * @copyright 2020 Jacques Marneweck.  All rights strictly reserved.
+ */
+
+declare(strict_types=1);
+
+namespace Jacques\SMS;
+
+class Version
+{
+    /**
+     * @var string
+     */
+    const VERSION = '0.0.1';
+}
+
