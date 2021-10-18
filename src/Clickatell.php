2ed21eceeec01ab4b2ee8761b00d7844714dec73
Jacques Marneweck <jacques@siberia.co.za>
2021-10-18 06:31:30 +0000

    Fetch guzzle config

--- src/Clickatell.php
+++ src/Clickatell.php
@@ -675,6 +675,24 @@ class Clickatell
             throw new \Exception($body);
         }
     }
+
+    /**
+     * Get a client configuration option.
+     *
+     * These options include default request options of the client, a "handler"
+     * (if utilized by the concrete client), and a "base_uri" if utilized by
+     * the concrete client.
+     *
+     * @param string|null $option The config option to retrieve.
+     *
+     * @return mixed
+     *
+     * @deprecated Client::getConfig will be removed in guzzlehttp/guzzle:8.0.
+     */
+    public function getConfig(?string $option = null)
+    {
+        return $this->client->getConfig($option);
+    }
 }
 
 /* vim: set noet ts=4 sw=4 ft=php: : */
